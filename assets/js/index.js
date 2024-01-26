3f4648bbf98f6f5ee0ae4605a015ddefb1bc041b
fqZell <mulkyway00@gmail.com>
2024-01-26 10:56:09 +0300

    add to footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,22 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 16,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    400: {
+      slidesPerView: 1,
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    600: {
+      slidesPerView: 2,
     },
+    1000: {
+      slidesPerView: 3,
+    },
+    1400: {
+      slidesPerView: 5,
+    },
+  }
 });
